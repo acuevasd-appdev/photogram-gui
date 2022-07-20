e894a09daa25883a7d4845d90f0aa77d03d51c91
acuevasd <acuevasd@chicagobooth.edu>
2022-07-20 23:13:47 +0000

    user index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index"})
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:path_id", { :controller => "users", :action => "show" })
 
+  get("/photos", { :controller => "photos", :action => "index"})
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
 end
